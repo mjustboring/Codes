b1b5f261ce6c97dbd20d3efad763eb5b2167264e
Sp Maurya <spm.kvmrn@yahoo.com>
2022-06-08 16:46:55 +0530

    Testing

---
+++ New Git File (1).cxx
@@ -0,0 +1,28 @@
+#include "/storage/emulated/0/Documents/Cxxdroid/headers"
+// #include <bits/stdc++.h>
+using namespace std;
+
+static const bool ___ = []() {
+    ios::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+	return 0;
+}();
+
+void solve() {
+    
+    cout << "Hello Git";
+    
+    cout << '\n';
+}
+
+
+int main() {
+	
+	int t = 1;
+	// cin >> t;
+	
+	while (t--) solve();
+	
+	return 0;
+}
